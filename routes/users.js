58bac598785032d2791277b2ca9c24be3056eed1
phonix <mrviit@gmail.com>
2020-12-25 02:03:55 -0800

    update

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router()
 const Joi = require('joi')
 const passport = require('passport')
+const { check, validationResult} = require("express-validator/check");
+const jwt = require("jsonwebtoken");
  
 const User = require('../models/user')
  
@@ -62,5 +64,78 @@ router.route('/register')
   .get((req, res) => {
     res.render('statistical')
   })
+
+///
+router.post(
+  "/signup",
+  [
+      check("username", "Please Enter a Valid Username")
+      .not()
+      .isEmpty(),
+      check("email", "Please enter a valid email").isEmail(),
+      check("password", "Please enter a valid password").isLength({
+          min: 6
+      })
+  ],
+  async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({
+              errors: errors.array()
+          });
+      }
+
+      const {
+          username,
+          email,
+          password
+      } = req.body;
+      try {
+          let user = await User.findOne({
+              email
+          });
+          if (user) {
+              return res.status(400).json({
+                  msg: "User Already Exists"
+              });
+          }
+
+          user = new User({
+              username,
+              email,
+              password
+          });
+
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(password, salt);
+
+          await user.save();
+
+          const payload = {
+              user: {
+                  id: user.id
+              }
+          };
+
+          jwt.sign(
+              payload,
+              "randomString", {
+                  expiresIn: 10000
+              },
+              (err, token) => {
+                  if (err) throw err;
+                  res.status(200).json({
+                      token
+                  });
+              }
+          );
+      } catch (err) {
+          console.log(err.message);
+          res.status(500).send("Error in Saving");
+      }
+  }
+);
+///
+
 
   module.exports = router
